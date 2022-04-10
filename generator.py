8840ea95609c54c857d81b15d01caaaf219590e8
arossell111 <arossell@lion.lmu.edu>
2022-04-10 14:29:33 -0700

    Generator

---
+++ generator.py
@@ -0,0 +1,27 @@
+import csv
+import random
+
+def get_data():
+    words = {}
+    with open('All-Pairs.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            if row[0] not in words:
+                words[row[0]] = [(row[1], float(row[2]))]
+            else:
+                words[row[0]].append((row[1], float(row[2])))
+    return words
+
+words = get_data()
+cue = None
+
+while cue == None:
+    cue = input("Enter your cue: ")
+    if cue in words:
+        print("Your cue is", cue)
+    else:
+        cue = None
+        print("Invalid cue")
+
+
+print(words[cue][random.randint(0, len(words[cue]))])
